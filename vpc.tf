7912d8b0103891aef967ee30f59c91b702dc3c8a
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-12-03 13:24:22 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,21 @@
+# Create a Resource Group if it doesn’t exist
+resource "azurerm_resource_group" "tfexample" {
+  name     = "my-terraform-rg"
+  location = "West Europe"
+}
+
+# Create a Virtual Network
+resource "azurerm_virtual_network" "tfexample" {
+  name                = "my-terraform-vnet"
+  location            = azurerm_resource_group.tfexample.location
+  resource_group_name = azurerm_resource_group.tfexample.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Create a Subnet in the Virtual Network
+resource "azurerm_subnet" "tfexample" {
+  name                 = "my-terraform-subnet"
+  resource_group_name  = azurerm_resource_group.tfexample.name
+  virtual_network_name = azurerm_virtual_network.tfexample.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
