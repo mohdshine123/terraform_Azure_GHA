c01a831ef09ec987a9218f08e0404da49d943c8d
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2023-12-03 15:16:25 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,6 +4,7 @@ terraform {
   required_version = ">= 0.12"
 
   backend "azurerm" {
+  use_oidc = true
     # Get backend configuration from config/backend-*.conf  # Initialise using: terraform init -backend-config=config/backend-dev.conf
   }
 }
