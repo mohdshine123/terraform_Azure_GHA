7986acc30fdc7bee673b269e07bf27ed670eceda
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2024-01-13 23:13:54 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -27,6 +27,7 @@ data "azurerm_virtual_network" "existing_vnet" {
 data "azurerm_subnet" "existing_subnet" {
   name                 = local.subnet_name
   virtual_network_name  = data.azurerm_virtual_network.existing_vnet.name
+  resource_group_name = data.azurerm_resource_group.existing_rg.name
 }
 
 
