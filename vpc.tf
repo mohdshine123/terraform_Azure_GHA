75691b186d4fdf0fd3ba6ba93b10ff0f3ff85b50
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2024-01-13 22:13:14 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -19,15 +19,15 @@ resource "azurerm_resource_group" "my-terraform-rg" {
 # Create a Virtual Network
 resource "azurerm_virtual_network" "my-terraform-vnet" {
   name                = "my-terraform-vnet"
-  location            = azurerm_resource_group.tfexample.location
-  resource_group_name = azurerm_resource_group.tfexample.name
+  location            = azurerm_resource_group.my-terraform-rg.location
+  resource_group_name = azurerm_resource_group.my-terraform-rg.name
   address_space       = ["10.0.0.0/16"]
 }
 
 # Create a Subnet in the Virtual Network
 resource "azurerm_subnet" "my-terraform-subnet" {
   name                 = "my-terraform-subnet"
-  resource_group_name  = azurerm_resource_group.tfexample.name
-  virtual_network_name = azurerm_virtual_network.tfexample.name
+  resource_group_name  = azurerm_resource_group.my-terraform-rg.name
+  virtual_network_name = azurerm_virtual_network.my-terraform-vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
