26c3a9bb2397dc42b009093cc994c4185a6dd9ed
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2024-01-13 23:00:00 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,8 @@
 locals {
-  resource_group_name = "my-terraform-rg"
-  location            = "Central India"
+  resource_group_name  = "my-terraform-rg"
+  virtual_network_name = "my-terraform-vnet"
+  subnet_name          = "my-terraform-subnet"
+  location             = "Central India"
 }
 
 # Check if the resource group already exists
@@ -17,31 +19,30 @@ resource "azurerm_resource_group" "my-terraform-rg" {
 
 # Check if the virtual network already exists
 data "azurerm_virtual_network" "existing_vnet" {
-  count               = azurerm_resource_group.my-terraform-rg[0].count
-  name                = "my-terraform-vnet"
-  resource_group_name = azurerm_resource_group.my-terraform-rg[0].name
+  name                = local.virtual_network_name
+  resource_group_name = data.azurerm_resource_group.existing_rg[0].name
 }
 
 # Check if the subnet already exists
 data "azurerm_subnet" "existing_subnet" {
-  count                = azurerm_virtual_network.existing_vnet[0].count
-  name                 = "my-terraform-subnet"
-  resource_group_name  = azurerm_resource_group.my-terraform-rg[0].name
-  virtual_network_name = azurerm_virtual_network.existing_vnet[0].name
+  name                 = local.subnet_name
+  resource_group_name  = data.azurerm_resource_group.existing_rg[0].name
 }
+
+
 
 # Create a Virtual Network and Subnet conditionally
 resource "azurerm_virtual_network" "my-terraform-vnet" {
-  count               = data.azurerm_subnet.existing_subnet ? 0 : 1
-  name                = "my-terraform-vnet"
+  count               = length(data.azurerm_virtual_network.existing_vnet) > 0 ? 0 : 1
+  name                = local.virtual_network_name
   location            = azurerm_resource_group.my-terraform-rg[0].location
   resource_group_name = azurerm_resource_group.my-terraform-rg[0].name
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "my-terraform-subnet" {
-  count                = data.azurerm_subnet.existing_subnet ? 0 : 1
-  name                 = "my-terraform-subnet"
+  count                = length(data.azurerm_subnet.existing_subnet) > 0 ? 0 : 1
+  name                 = local.subnet_name
   resource_group_name  = azurerm_resource_group.my-terraform-rg[0].name
   virtual_network_name = azurerm_virtual_network.my-terraform-vnet[0].name
   address_prefixes     = ["10.0.2.0/24"]
