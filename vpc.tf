45a37184dcebd228cfa1c71193289146cfbb8714
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2024-01-15 02:39:47 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,8 @@ resource "azurerm_resource_group" "network" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.vnet_name}"
-  location            = "${var.location}"
+  #location            = "${var.location}"
+  location             = "${local.location}"
   address_space       = ["${var.address_space}"]
   resource_group_name = "${azurerm_resource_group.network.name}"
   dns_servers         = "${var.dns_servers}"
