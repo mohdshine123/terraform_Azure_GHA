6498ba8d1c4766c58516e7d953c3b7bffde53554
mohdshine123 <139389575+mohdshine123@users.noreply.github.com>
2024-01-15 02:57:30 +0530

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,5 +1,6 @@
 locals {
-  resource_group_name  = "myapp-rg"
+  rgnamedev            =  "myapp-rg"
+  #resource_group_name  = "myapp-rg"
   virtual_network_name = "my-terraform-vnet"
   subnet_name          = "my-terraform-subnet"
   location             = "centralindia"
